780060102aa2a631da026b8471a9ebb372c716b4
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-08 11:27:55 +0200

    new component
    tips component

--- classroom/src/App.tsx
+++ classroom/src/App.tsx
@@ -1,6 +1,8 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
 import { Tip } from "./components/Tip";
+import { Letter } from "./components/Letter";
+import { Input } from "./components/Input";
 
 function App() {
   function handleRestartGame() {
@@ -14,6 +16,18 @@ function App() {
           tip="Dynamic programming language
 "
         />
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Guess</h4>
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
+        </div>
       </main>
     </div>
   );
