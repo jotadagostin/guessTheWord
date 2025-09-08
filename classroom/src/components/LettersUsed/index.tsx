a509984421e5e08347dcc15aefaae689c7c3db57
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-09-08 16:07:24 +0200

    setting letter colors

--- classroom/src/components/LettersUsed/index.tsx
+++ classroom/src/components/LettersUsed/index.tsx
@@ -1,9 +1,15 @@
+import { Letter } from "../Letter";
 import styles from "./styles.module.css";
 
 export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Lettes used</h5>
+
+      <div>
+        <Letter value="X" size="small" color="correct" />
+        <Letter value="R" size="small" color="wrong" />
+      </div>
     </div>
   );
 }
